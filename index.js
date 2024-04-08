a2f72b1d92f1c7eb855a0a8d63d4b6aee4e2ed38
maximelfourier <166380764+maximelfourier@users.noreply.github.com>
2024-04-08 11:15:44 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Surveille l'établissement","Inspecte vos candidatures"];
 
 
 let currentIndex = 0;
